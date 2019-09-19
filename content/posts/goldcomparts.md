e268180ed37c27295c941dd24a8e8c0df3a29193
hmorl <harry.morley@live.co.uk>
2019-09-19 10:29:09 +0100

    rebuilding site Thu 19 Sep 2019 10:29:03 BST

--- content/posts/goldcomparts.md
+++ content/posts/goldcomparts.md
@@ -11,6 +11,6 @@ Map implemented by Julia Makivic.
 
 Built using [Hugo](https://gohugo.io), the static site generator, HTML/CSS3 and JavaScript (no libraries). Makes use of a monospace font and the "ch" unit in CSS to perfectly align the ASCII art design to the grid.
 
-Visit [goldcomparts.show](http://goldcomparts.show)
+Visit [goldcomparts.show](https://goldcomparts.show)
 
-<iframe src="http://goldcomparts.show" width="100%" height="500px" frameborder="0"></iframe>
+<iframe src="https://goldcomparts.show" width="100%" height="500px" frameborder="0"></iframe>
--- deploy.sh
+++ deploy.sh
@@ -5,30 +5,48 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-cd public
+# Build the project in temporary public folder
+hugo
 
+# Go to public repo
+cd ../hmorl.github.io
+
+# Remove everything except hidden .git files
 rm -r *
 
-cd ..
+# Go back to source
+cd ../website-source
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Copy files from public to the public repo
+cp -a public/. ../hmorl.github.io/
 
-# Go To Public folder
-cd public
+# Remove the temp public folder
+rm -rf public
 
-# Add changes to git.
+# Go to public repo
+cd ../hmorl.github.io
+
+# Add changes to git
 git add .
 
-# Commit changes.
+# Commit changes
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push source and build repos
 git push origin master
 
-# Come Back up to the Project Root
-cd ..
+# Go back to website source
+cd ../website-source
+
+# Add source changes to git
+git add .
+
+# Commit source changes
+git commit -m "$msg"
+
+# Push source
+git push origin master
