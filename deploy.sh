19348b9f35e9b5e623533e7e0fd0181b203244a7
hmorl <harry.morley@live.co.uk>
2019-09-19 09:58:13 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+cd public
+
+rm -r *
+
+cd ..
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
+
 # Add changes to git.
 git add .
 
